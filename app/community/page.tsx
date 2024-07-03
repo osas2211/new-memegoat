b22dae4efc39885f038befb307193673f32bc5b4
Osaretin Frank <osaretin.frank10@gmail.com>
2024-07-03 10:22:56 +0100

    add updates

---
+++ app/community/page.tsx
@@ -0,0 +1,12 @@
+import { Community } from "@/components/community/Community"
+import React from "react"
+
+const CommunityPage = () => {
+  return (
+    <div>
+      <Community />
+    </div>
+  )
+}
+
+export default CommunityPage
