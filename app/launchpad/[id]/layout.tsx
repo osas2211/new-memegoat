1b71734b47d6e1c62d12ccc121eb09fa5f2d316f
JoE11-y <Josephedoh77@gmail.com>
2024-08-07 17:17:13 +0100

    update

--- app/launchpad/[id]/layout.tsx
+++ app/launchpad/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export const dynamicParams = false
+export const dynamicParams = false;
+export const runtime = 'edge';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
