a7f6609d92217acf3d7b2b007369980f01aa953a
JoE11-y <Josephedoh77@gmail.com>
2024-08-07 22:32:09 +0100

    updates

--- app/staking/page.tsx
+++ app/staking/page.tsx
@@ -5,13 +5,13 @@ import React from "react"
 
 const StakingPage = () => {
   // Set a specific target date (e.g., August 15, 2024)
-  const targetDate = '2024-08-12T16:00:00Z'
+  // const targetDate = '2024-08-12T16:00:00Z'
   return (
     <div>
-      <CountdownTimer targetDate={targetDate} />
-      {/* <div className="flex items-center justify-center mt-10">
+      {/* <CountdownTimer targetDate={targetDate} /> */}
+      <div className="flex items-center justify-center mt-10">
         <MemeGoatStakingTab />
-      </div> */}
+      </div>
     </div>
   )
 }
