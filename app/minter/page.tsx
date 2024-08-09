d5ca9ae3d76a809daeab3e071f0019833b6c9c42
JoE11-y <Josephedoh77@gmail.com>
2024-08-09 18:44:20 +0100

    open minter

--- app/minter/page.tsx
+++ app/minter/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { MinterPage } from "@/components/minter/MinterPage"
-import { CountdownTimer } from "@/components/shared/CountdownTimer"
+// import { CountdownTimer } from "@/components/shared/CountdownTimer"
 import React from "react"
 
 const MintingPage = () => {
-  const targetDate = "2024-08-12T16:00:00Z"
+  // const targetDate = "2024-08-12T16:00:00Z"
   return (
     <div>
-      {/* <MinterPage /> */}
-      <CountdownTimer targetDate={targetDate} />
+      <MinterPage />
+      {/* <CountdownTimer targetDate={targetDate} /> */}
     </div>
   )
 }
